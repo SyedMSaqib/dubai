1eb7184c7747d5854a7d0ce2d5518633c77c8713
Saqib <splasher733@gmail.com>
2024-09-17 01:26:59 +0500

    optimize: image sizes optimized for different screen sizes.

--- components/ui/ImageCarousel.tsx
+++ components/ui/ImageCarousel.tsx
@@ -79,6 +79,7 @@ const ImageCarousel = () => {
                 }}
                 draggable="false"
                 priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
           ))}
