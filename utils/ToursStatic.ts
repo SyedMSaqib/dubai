4c8986bbb6bb6cf3b5b84dbd7c2abfaa73e462ee
Saqib <splasher733@gmail.com>
2024-09-20 22:09:04 +0500

    add: file for static data

---
+++ utils/ToursStatic.ts
@@ -0,0 +1,59 @@
+export const tourPackages = [
+    { src: "/images/desertSafari.jpg", text: "Desert Safari" },
+    { src: "/images/heliride.jpg", text: "Helicopter Ride" },
+    { src: "/images/dowCuise.jpg", text: "Dhow Cruise Dinner" },
+    { src: "/images/burjKhalifa.jpg", text: "Burj Khalifa Tour" },
+    { src: "/images/dubaiCity.jpg", text: "Dubai City Tour" },
+    { src: "/images/atlantas.jpg", text: "Atlantis Aquaventure" },
+    { src: "/images/hotAir.jpg", text: "Hot Air Balloon Ride" },
+    { src: "/images/marinaYacht.jpg", text: "Dubai Marina Yacht Cruise" },
+    { src: "/images/garden.jpg", text: "Dubai Miracle Garden" },
+    { src: "/images/dubai5.jpg", text: "Dubai Frame Experience" },
+    { src: "/images/dubai1.jpg", text: "Ski Dubai Adventure" },
+    { src: "/images/dubai2.jpg", text: "Ferrari World Tour" },
+  ]
+  
+  export const topTours = [
+    {
+      src: "/images/desertSafari.jpg",
+      price: 200,
+      title: "Desert Safari",
+      rating: 0,
+      totalRatings: 0,
+    },
+    {
+      src: "/images/heliride.jpg",
+      price: 299.99,
+      title: "Helicopter Ride",
+      rating: 2,
+      totalRatings: 148,
+    },
+    {
+      src: "/images/dowCuise.jpg",
+      price: 322,
+      title: "Dhow Cruise Dinner",
+      rating: 3.5,
+      totalRatings: 234,
+    },
+    {
+      src: "/images/burjKhalifa.jpg",
+      price: 130,
+      title: "Burj Khalifa Tour",
+      rating: 5,
+      totalRatings: 100,
+    }, // Add totalRatings
+    {
+      src: "/images/dubaiCity.jpg",
+      price: 180,
+      title: "Dubai City Tour",
+      rating: 4.5,
+      totalRatings: 87,
+    },
+    {
+      src: "/images/atlantas.jpg",
+      price: 499.9,
+      title: "Atlantis Aquaventure",
+      rating: 4.5,
+      totalRatings: 29,
+    },
+  ]
