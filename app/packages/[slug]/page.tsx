10e40ac235eb68bc21409b01b2442d5fd0c24429
Saqib <splasher733@gmail.com>
2024-09-21 04:56:24 +0500

    ...

--- app/packages/[slug]/page.tsx
+++ app/packages/[slug]/page.tsx
@@ -1,5 +1,7 @@
-import PackageSidebar from "@/components/ui/PackageSidebar/PackageSidebar"
-import MobilePackageFilter from "@/components/ui/PackageSidebar/MobilePackageFilter"
+import PackageSidebar from "@/components/ui/MainTourPackages/PackageSidebar/PackageSidebar"
+import MobilePackageFilter from "@/components/ui/MainTourPackages/PackageSidebar/MobilePackageFilter"
+import PackagesItem from "@/components/ui/MainTourPackages/PackagesItem"
+
 const Packages = ({ params }: { params: { slug: string } }) => {
   const { slug } = params
   const slugToText = (slug: string) => {
@@ -7,14 +9,27 @@ const Packages = ({ params }: { params: { slug: string } }) => {
   }
   const slugg = slugToText(slug)
   return (
-    <div className="mx-auto  lg:max-w-[70vw] px-4 sm:px-6 lg:px-8 mt-10">
-      <h3 className="  text-xl sm:text-2xl lg:text-3xl font-bold">{slugg}</h3>
+    <div className="mx-auto lg:max-w-[70vw] px-4 sm:px-6 lg:px-8 mt-10">
+      <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold">{slugg}</h3>
       <div>
         <MobilePackageFilter />
       </div>
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-col md:flex-row gap-5 mt-5">
+        {/* Sidebar */}
         <div className="hidden md:block">
           <PackageSidebar />
+        </div>
+
+        {/* PackagesItem and additional content */}
+        <div className="flex flex-col gap-5 ">
+          <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg p-4">
+            <div className="flex-shrink-0 w-[300px]">
+              <PackagesItem />
+            </div>
+            <div className="">
+              Las Vegas Helicopter Night Flight and Optional VIP Transportationasdasdasdasdasd
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/ui/MainTourPackages/PackagesItem.tsx
@@ -0,0 +1,21 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card"
+import Image from "next/image"
+
+const PackagesItem = () => {
+  return (
+    <Card className="shadow-none rounded-lg">
+      <CardBody className="overflow-visible p-0">
+        <Image
+          alt="image"
+          className="w-full object-cover h-[190px] md:h-[240px]"
+          src="/images/marinaYacht.jpg"
+          width={600}
+          height={400}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </CardBody>
+    </Card>
+  )
+}
+
+export default PackagesItem
