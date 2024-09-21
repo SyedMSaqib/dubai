c59a05dfda9b30863d0ba2b734078f10aac3e564
Saqib <splasher733@gmail.com>
2024-09-21 05:44:07 +0500

    fix: packages layout

--- app/packages/[slug]/page.tsx
+++ app/packages/[slug]/page.tsx
@@ -21,7 +21,7 @@ const Packages = ({ params }: { params: { slug: string } }) => {
         </div>
 
         {/* PackagesItem and additional content */}
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -35,8 +35,19 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
+          <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
+            <div className="flex-shrink-0  w-[150px] sm:w-[300px]">
+              <PackagesItem />
+            </div>
+            <div className="flex flex-col pt-2 gap-y-2 pr-4">
+              <p className="text-sm">4.5 (1221)</p>
+              <p className=" text-sm font-semibold">
+                Las Vegas Helicopter Night Flight and Optional VIP Transportation
+              </p>
+              <p className="text-sm">1 to 3 hrs</p>
+              <p className=" text-end font-bold text-medium">$500</p>
+            </div>
+          </div>
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -50,8 +61,6 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -65,8 +74,6 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -80,8 +87,6 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -95,8 +100,6 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
@@ -110,38 +113,6 @@ const Packages = ({ params }: { params: { slug: string } }) => {
               <p className=" text-end font-bold text-medium">$500</p>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
-          <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
-            <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
-              <PackagesItem />
-            </div>
-            <div className="flex flex-col pt-2 gap-y-2 pr-4">
-              <p className="text-sm">4.5 (1221)</p>
-              <p className=" text-sm font-semibold">
-                Las Vegas Helicopter Night Flight and Optional VIP Transportation
-              </p>
-              <p className="text-sm">1 to 3 hrs</p>
-              <p className=" text-end font-bold text-medium">$500</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
-          <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
-            <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
-              <PackagesItem />
-            </div>
-            <div className="flex flex-col pt-2 gap-y-2 pr-4">
-              <p className="text-sm">4.5 (1221)</p>
-              <p className=" text-sm font-semibold">
-                Las Vegas Helicopter Night Flight and Optional VIP Transportation
-              </p>
-              <p className="text-sm">1 to 3 hrs</p>
-              <p className=" text-end font-bold text-medium">$500</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-5 ">
           <div className="flex flex-row gap-5 w-full border border-zinc-300 rounded-lg sm:p-4">
             <div className="flex-shrink-0 w-[150px] sm:w-[300px]">
               <PackagesItem />
