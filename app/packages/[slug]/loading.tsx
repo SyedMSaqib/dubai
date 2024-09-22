6c9dd7a43ab4201acc9da9ae332baf2ffd151f33
Saqib <splasher733@gmail.com>
2024-09-23 02:17:18 +0500

    fix: package loader

--- app/packages/[slug]/loading.tsx
+++ app/packages/[slug]/loading.tsx
@@ -1,65 +1,25 @@
 import { Skeleton } from "@nextui-org/skeleton"
-import { Card } from "@nextui-org/card"
 
 export default function Loading() {
   return (
-    <div className="mx-auto  2xl:max-w-[90vw] px-4 sm:px-6 lg:px-8 mt-10 mb-10">
-      <Skeleton className=" w-1/5 rounded-lg lg:text-3xl">
+    <div className="mx-auto 2xl:max-w-[90vw] px-4 sm:px-6 lg:px-8 mt-10 mb-10">
+      <Skeleton className="w-1/5 rounded-lg lg:text-3xl">
         <div className="h-5 w-2/5 rounded-lg bg-default-300"></div>
       </Skeleton>
-      <div className="lg:mt-[20px] mt-[60px] flex flex-col gap-2 w-full">
-        <div className="flex gap-2">
-          <Skeleton className=" w-[170px] h-[170px] rounded-lg lg:text-3xl"></Skeleton>
-          <div className="space-y-4 mt-[50px]">
-            <Skeleton className=" w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <Skeleton className=" w-[170px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <div className="flex justify-between pt-[20px]">
-              <Skeleton className=" w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-              <Skeleton className=" w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <Skeleton className=" w-[170px] h-[170px] rounded-lg lg:text-3xl"></Skeleton>
-          <div className="space-y-4 mt-[50px]">
-            <Skeleton className=" w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <Skeleton className=" w-[170px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <div className="flex justify-between pt-[20px]">
-              <Skeleton className=" w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-              <Skeleton className=" w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <Skeleton className=" w-[170px] h-[170px] rounded-lg lg:text-3xl"></Skeleton>
-          <div className="space-y-4 mt-[50px]">
-            <Skeleton className=" w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <Skeleton className=" w-[170px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <div className="flex justify-between pt-[20px]">
-              <Skeleton className=" w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-              <Skeleton className=" w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <Skeleton className=" w-[170px] h-[170px] rounded-lg lg:text-3xl"></Skeleton>
-          <div className="space-y-4 mt-[50px]">
-            <Skeleton className=" w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <Skeleton className=" w-[170px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <div className="flex justify-between pt-[20px]">
-              <Skeleton className=" w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-              <Skeleton className=" w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <Skeleton className=" w-[170px] h-[170px] rounded-lg lg:text-3xl"></Skeleton>
-          <div className="space-y-4 mt-[50px]">
-            <Skeleton className=" w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <Skeleton className=" w-[170px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-            <div className="flex justify-between pt-[20px]">
-              <Skeleton className=" w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
-              <Skeleton className=" w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
+      <div className="flex lg:mt-[20px] mt-[60px] flex-row gap-4">
+        <Skeleton className="hidden lg:block lg:w-[300px] w-[200px] h-screen rounded-lg lg:text-3xl"></Skeleton>
+        <div className="flex-grow">
+          <div className="flex gap-2">
+            <Skeleton className="w-[200px] md:w-[350px] md:p-6 h-[170px] md:h-[250px] rounded-lg lg:text-3xl"></Skeleton>
+            <div className="space-y-4 mt-[50px] w-full">
+              <Skeleton className="w-[70px] h-3 rounded-lg lg:text-3xl"></Skeleton>
+
+              <Skeleton className="w-full h-3 rounded-lg lg:text-3xl"></Skeleton>
+              <Skeleton className="w-full h-3 rounded-lg lg:text-3xl"></Skeleton>
+              <div className="flex justify-between pt-[20px]">
+                <Skeleton className="w-[50px] h-3 rounded-lg lg:text-3xl"></Skeleton>
+                <Skeleton className="w-[40px] h-3 rounded-lg lg:text-3xl"></Skeleton>
+              </div>
             </div>
           </div>
         </div>
