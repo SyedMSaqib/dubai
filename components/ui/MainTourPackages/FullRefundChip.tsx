bed9010074d83448fa2d510a3dbcd0c039b816d8
Saqib <splasher733@gmail.com>
2024-09-26 00:01:35 +0500

    ...

--- components/ui/MainTourPackages/FullRefundChip.tsx
+++ components/ui/MainTourPackages/FullRefundChip.tsx
@@ -1,13 +1,10 @@
+import { Tick } from "@/utils/StaticSvgs"
+
 export default function FullRefundChip() {
   return (
     <div className="bg-green-100 rounded-lg p-4">
       <div className="flex  gap-2">
-        <div className="pt-1">
-          <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="8" fill="#74DFA2" />
-            <path d="M6 10 L10 14 L14 6" stroke="white" strokeWidth="2" fill="none" />
-          </svg>
-        </div>
+        <div className="pt-1">{Tick}</div>
         <p className="flex-1">
           <span className="underline hover:cursor-pointer pr-1">Free cancellation </span> full
           refund available for cancellations made up to 24 hours prior to the start of the
@@ -16,12 +13,8 @@ export default function FullRefundChip() {
       </div>
       {/* 2nd */}
       <div className="flex  gap-2">
-        <div className="pt-1">
-          <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="8" fill="#74DFA2" />
-            <path d="M6 10 L10 14 L14 6" stroke="white" strokeWidth="2" fill="none" />
-          </svg>
-        </div>
+        <div className="pt-1">{Tick}</div>
+
         <p className="flex-1">
           <span className="underline hover:cursor-pointer pr-1">Lowest price guarantee </span>We
           ensure you receive the best available rate, backed by our price match guarantee.
@@ -29,12 +22,8 @@ export default function FullRefundChip() {
       </div>
       {/* 3rd */}
       <div className="flex  gap-2">
-        <div className="pt-1">
-          <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="8" fill="#74DFA2" />
-            <path d="M6 10 L10 14 L14 6" stroke="white" strokeWidth="2" fill="none" />
-          </svg>
-        </div>
+        <div className="pt-1">{Tick}</div>
+
         <p className="flex-1">
           <span className="underline hover:cursor-pointer pr-1">Pay Now or Upon Arrival</span>Book
           now and pay when you arrive for added convenience and flexibility.
