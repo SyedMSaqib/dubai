06e340947c3a0a1e20558a898ceb7889e6278044
Saqib <splasher733@gmail.com>
2024-09-26 22:41:59 +0500

    ..

--- components/ui/MainTourPackages/TourInclusions.tsx
+++ components/ui/MainTourPackages/TourInclusions.tsx
@@ -7,7 +7,7 @@ const TourInclusions = () => {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
   return (
-    <Accordion selectionMode="multiple">
+    <Accordion defaultSelectedKeys="all" selectionMode="multiple">
       <AccordionItem key="6" aria-label="Accordion 6" title={<strong>Highlights</strong>}>
         <div className="flex">
           <div>{Dot}</div>
