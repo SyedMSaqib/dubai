d6f3975b4538437c805333cad7e0687ce9c79ff0
Saqib <splasher733@gmail.com>
2024-10-01 00:47:04 +0500

    ...

--- app/checkout/[data]/page.tsx
+++ app/checkout/[data]/page.tsx
@@ -5,13 +5,14 @@ import ContactForm from "@/components/ui/Checkout/ContactForm"
 import BreadCrumbs from "@/components/ui/Checkout/BreadCrumbs"
 import StripeElements from "@/components/ui/Checkout/StripeElements"
 import { useAppSelector } from "@/lib/Redux/hooks"
-// import { decodeData } from "@/utils/urlEncoders"
+import { decodeData } from "@/utils/urlEncoders"
 
 export default function Checkout({ params }: { params: { data: string } }) {
-  // const { data } = params
+  const { data } = params
 
   // Decode the data
-  // const decodedData = decodeData(data)
+  const decodedData = decodeData(data)
+  console.log(decodedData)
 
   const currentPage = useAppSelector((state) => state.formData.setCurrentPage)
 
